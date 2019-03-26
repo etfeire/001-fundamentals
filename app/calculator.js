2684e5222493836a9aa5f89d2bfc68ac61124d5b
etfeire <etfeire@gmail.com>
2019-03-26 20:11:23 +0000

    first commit

--- app/calculator.js
+++ app/calculator.js
@@ -2,6 +2,45 @@ function add(a, b) {
     return a + b;
 }
 
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function isOdd(a) {
+    if (a % 2 == 1)
+        return true
+    else
+        return false
+}
+
+function isEven(a) {
+    if (a % 2 == 0)
+        return true
+    else
+        return false
+}
+
+function divisibleBy(a, b) {
+    if (a % b == 0) 
+        return true
+    else
+        return false
+}
+
+function squared(a) {
+    return a*a
+}
+
 module.exports = {
-    add
+    add,
+    subtract,
+    multiply,
+    isOdd,
+    isEven,
+    divisibleBy,
+    squared 
 };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,27 @@ const calculator = require('../app/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('subtracts 5 - 3 to equal 2', () => {
+  expect(calculator.subtract(5, 3)).toBe(2);
+});
+
+test('multiplies 4  * 2 to equal 8', () => {
+  expect(calculator.multiply(4, 2)).toBe(8);
+});
+
+test('checks if 65 is odd', () => {
+  expect(calculator.isOdd(65)).toBe(true);
+});
+
+test('checks if 65 is even', () => {
+  expect(calculator.isEven(65)).toBe(false);
+});
+
+test('checks if first number is evenly divisible by the second', () => {
+  expect(calculator.divisibleBy(65, 5)).toBe(true);
+});
+
+test('checks square of 5 to equal 25', () => {
+  expect(calculator.squared(5)).toBe(25);
+});
